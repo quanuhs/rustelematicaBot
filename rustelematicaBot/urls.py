91c7f5c5afca34ceddfec3bb79bb5baa53cee0ca
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-10 16:33:57 +0300

    added basic functions

--- rustelematicaBot/urls.py
+++ rustelematicaBot/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import telegramBot.views as telegram_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('telegram/<secret_key>', telegram_view.handle_telegram)
 ]
--- telegramBot/views.py
+++ telegramBot/views.py
@@ -1,3 +1,20 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def handle_telegram(request, secret_key):
+    #data = request.body.decode('utf-8')
+    data = json.loads(request.body.decode('utf-8'))
+
+    if 'callback_query' in data:
+        #messages.handler_call_back(data)
+        pass
+
+    elif 'message' in data:
+        pass
+
+    return HttpResponse('ok', content_type="text/plain", status=200)
