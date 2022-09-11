b68329eea821b0d6e930b84ddb3ab145955b9e9e
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-11 16:59:39 +0300

    update

--- rustelematicaBot/settings.py
+++ rustelematicaBot/settings.py
@@ -125,4 +125,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*"]
--- telegramBot/admin.py
+++ telegramBot/admin.py
@@ -3,21 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from telegramBot.models import BotDictionary, BotSettings
 
+
 @admin.register(BotDictionary)
 class BotDictionaryAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Локализация', {
-            'fields': ('language')
+            'fields': ['language',]
         }),
         ('Авторизация', {
-            'fields': ('auth_text', 'auth_success', 'auth_ask_login', 'auth_fail_login', 'auth_ask_password',
-                       'auth_fail_password', 'menu_btn_login')
+            'fields': ['auth_text', 'auth_success', 'auth_ask_login', 'auth_fail_login', 'auth_ask_password',
+                       'auth_fail_password', 'menu_btn_login']
         }),
         ('Основное меню', {
-            'fields': ('menu_text', 'menu_btn_check', 'menu_btn_status', 'menu_btn_logout')
+            'fields': ['menu_text', 'menu_btn_check', 'menu_btn_status', 'menu_btn_logout']
         }),
         ('Прочее', {
-            'fields': ('welcome_text', 'error_no_command')
+            'fields': ['welcome_text', 'error_no_command']
         })
     )
 
