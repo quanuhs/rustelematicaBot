3d02d8c86deaaf21947efd53335d36bc30ef1258
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-11 15:43:17 +0300

    update allowed hosts

--- rustelematicaBot/settings.py
+++ rustelematicaBot/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-2zfxw4cqy98h^)#3vjb7y3on3u)(^&jy3yqb+!do&2no)=4g73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
