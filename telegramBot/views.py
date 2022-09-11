569232c0defdd70aa7ee2a54c15b7b149ec9b57a
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-11 16:17:55 +0300

    update

--- telegramBot/views.py
+++ telegramBot/views.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def handle_telegram(request, secret_key):
     data = request.body.decode('utf-8')
-    #data = json.loads(request.body.decode('utf-8'))
+    print(data)
 
     if 'callback_query' in data:
         #messages.handler_call_back(data)
