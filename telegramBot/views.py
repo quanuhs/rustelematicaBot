70e55fde55630a8774658a6c3073dcc692d93005
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-11 15:48:40 +0300

    update

--- telegramBot/views.py
+++ telegramBot/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def handle_telegram(request, secret_key):
-    #data = request.body.decode('utf-8')
-    data = json.loads(request.body.decode('utf-8'))
+    data = request.body.decode('utf-8')
+    #data = json.loads(request.body.decode('utf-8'))
 
     if 'callback_query' in data:
         #messages.handler_call_back(data)
