c89de3deae305690276197fbad36b56fe2b292f0
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-09-11 17:59:42 +0300

    update

--- telegramBot/messages.py
+++ telegramBot/messages.py
@@ -1,25 +1,17 @@
 import telebot
 from .models import BotSettings
 
-settings = BotSettings.objects.filter().first()
-
-if settings is not None:
-    bot = telebot.TeleBot(settings.token)
-else:
-    bot = telebot.TeleBot(None)
-
 
 def handle_message(request):
-    # global bot
-    #
-    # new_settings = BotSettings.objects.filter().first()
-    #
-    # if new_settings is not None:
-    #     bot = telebot.TeleBot(settings.token)
+    settings = BotSettings.objects.filter().first()
+
+    if settings is not None:
+        bot = telebot.TeleBot(settings.token)
+    else:
+        return
 
     bot.process_new_updates([telebot.types.Update.de_json(request.body.decode("utf-8"))])
 
-
-@bot.message_handler(content_types=['text'])
-def directives(message: telebot.types.Message):
-    bot.send_message(message.from_user.id, "cool")
+    @bot.message_handler(content_types=['text'])
+    def directives(message: telebot.types.Message):
+        bot.send_message(message.from_user.id, "cool")
